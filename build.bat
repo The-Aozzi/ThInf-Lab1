88912d19cfd090384b9905bc26bd1265ede99cd9
The-Aozzi <theaozzixd@gmail.com>
2024-09-07 16:34:07 +0500

    Add build scripts.

---
+++ build.bat
@@ -0,0 +1,2 @@
+cmake -S . -B build -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++
+cmake --build build
---
+++ build.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cmake -S. -B build -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++;
+cmake --build build;
